1804c1eb6fdd6b4214edaf30df25252cea87b1f1
han-yejin <biziyejin8844@naver.com>
2026-05-22 21:38:05 +0900

    test: mock S3 service in context load

--- src/test/java/com/yejin/ch4/Ch4ApplicationTests.java
+++ src/test/java/com/yejin/ch4/Ch4ApplicationTests.java
@@ -1,10 +1,17 @@
 package com.yejin.ch4;
 
+import com.yejin.ch4.image.S3ImageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Ch4ApplicationTests {
+
+	@MockitoBean
+	private S3ImageService s3ImageService;
 
 	@Test
 	void contextLoads() {
